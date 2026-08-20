0ac6aeeb7d25bc2046406d728d7e20fa8970a89d
kumawatdilip8878-dotcom <kumawatdilip8878@gmail.com>
2026-08-20 17:41:36 +0530

    Change in dashboard controller

--- src/controller/dashboardController.js
+++ src/controller/dashboardController.js
@@ -1,5 +1,5 @@
 const Product = require("../models/Product");
-const User = require("../models/user");
+const User = require("../models/User");
 
 // ==========================================
 // DASHBOARD
