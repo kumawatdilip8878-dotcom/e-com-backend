f17fe1f289716fd92506375a1ec0ccc2fac871e6
kumawatdilip8878-dotcom <kumawatdilip8878@gmail.com>
2026-08-20 17:30:34 +0530

    Fix user model import

--- src/Config/db.js
+++ src/Config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {await mongoose.connect(process.env.MONGO_URI),{
     dbName:"ecomarece"
   }
-    await mongoose.connect(process.env.MONGO_URI);
+    
 
     console.log("mongo connect");
   } catch (error) {
