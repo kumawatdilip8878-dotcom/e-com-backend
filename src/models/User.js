ddfa9334b82918953703b66b98abacdec301e95a
kumawatdilip8878-dotcom <kumawatdilip8878@gmail.com>
2026-08-21 16:52:56 +0530

    psuh

--- src/models/User.js
+++ src/models/User.js
@@ -14,15 +14,23 @@ const userSchema = new mongoose.Schema(
     },
 
     email: {
-  type: String,
-  required: true,
-  unique: true,
-  trim: true,
-  lowercase: true,
-},
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
     password: {
       type: String,
       required: true,
+    },
+
+    // USER ROLE
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
 
     otp: {
